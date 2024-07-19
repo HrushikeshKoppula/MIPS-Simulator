c2a329fbe272e0429578fe9ad65fbb215ff845c3
HrushikeshKoppula <hrushikeshkoppula@gmail.com>
2024-07-19 07:03:09 +0000

    it works, need to make seperate file now

--- ALU.cpp
+++ ALU.cpp
@@ -7,18 +7,22 @@ class ALU{
         // bool A[32];
         // bool B[32];
         // bool CarryIn;
-        bool Result[32];
-        bool CarryOut;
-        ONEBITALU OneBitALU[32];
-        ALU(){}
-        ALU(bool A[32],bool B[32],bool CarryIn,bool Binv,int OP){
+        // bool Result[32];
+        // bool CarryOut;
+        // ONEBITALU OneBitALU[32];
+        // ALU(){}
+        ALU(bool A[32],bool B[32],bool CarryIn,bool Binv,int OP,bool Result[32],bool &CarryOut){
             ONEBITALU::setSignals(Binv,OP);
-            for(int i=0;i<32;i++){
+            for(int i=31;i>=0;i--){
                 // this->A[i]=A[i];
                 // this->B[i]=B[i];
-                OneBitALU[i]=ONEBITALU(A[i],B[i],CarryIn);
-                Result[i]=OneBitALU[i].Result;
-                CarryIn=OneBitALU[i].CarryOut;
+                // OneBitALU[i]=;
+                ONEBITALU OneBitALU(A[i],B[i],CarryIn);
+                OneBitALU.RUN();
+                Result[i]=OneBitALU.Result;
+                CarryIn=OneBitALU.CarryOut;
+                // Debugging output
+                // std::cout << "Step " << i << ": Result = " << Result[i] << ", CarryOut = " << CarryOut << std::endl;
             }
         }
 };
@@ -26,21 +30,37 @@ class ALU{
 void converter(bool A[32],int n){
     std::bitset<32> bits(n);
     for(int i=0;i<32;i++){
-        A[i]=bits[i];
+        A[i]=bits[31-i];
     }
 }
 
+int converter(bool A[32]){
+    int n=-A[0];
+    for(int i=1;i<32;i++){
+        n=2*n+A[i];
+    }
+    return n;
+}
+
 int main(){
-    // ONEBITALU ALU0(1,1,0);
-    // ONEBITALU::setSignals(true,2);
-    // ALU0.RUN();
-    // std::cout<<ALU0.Result<<" "<<ALU0.CarryOut<<" ";
-    ALU Alu;
     bool Result[32];
+    bool CarryOut=false;
     bool A[32];
     bool B[32];
-    converter(A,32);
-    converter(B,32);
-    Result=Alu(A,B,0).Result;
+    bool CarryIn=false;
+    converter(A,2147483647);
+    converter(B,-2147483648);
+    // Debuggin converters
+    std::cout<<converter(A)<<"\n";
+    std::cout<<converter(B)<<"\n";
+    for(int i=0;i<32;i++)   std::cout<<A[i];
+    std::cout<<"\n";
+    for(int i=0;i<32;i++)   std::cout<<B[i];
+    std::cout<<"\n";
+    bool Binv=false;
+    int OP=2;
+    ALU(A,B,CarryIn,Binv,OP,Result,CarryOut);
+    int result=converter(Result);
+    std::cout<<result<<" "<<CarryOut<<"\n";
     return 0;
 }
